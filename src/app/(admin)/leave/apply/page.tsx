a3285ba0cafe1af203a90531c8784edcc89d517d
Pascal-Ahmadu <pascalally41@gmail.com>
2026-05-23 16:05:21 +0100

    dods

--- src/app/(admin)/leave/apply/page.tsx
+++ src/app/(admin)/leave/apply/page.tsx
@@ -11,13 +11,16 @@ export default function ApplyForLeavePage() {
     };
 
     return (
+        /**
+         * 
+         */
         <div>
             <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
                 <h2 className="text-title-md2 font-bold text-black dark:text-white">
                     Apply For Leave
                 </h2>
                 <nav>
-                    <ol className="flex items-center gap-2">
+                    <ol className="flex items-center gap-2 animate" >
                         <li>
                             <a className="font-medium" href="/">
                                 Dashboard /
